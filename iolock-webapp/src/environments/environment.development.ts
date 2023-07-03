e2802dcfe341e6933579f2606ac04136cbf93e9f
dalpontp <pietrodalpont@gmail.com>
2023-07-03 16:10:15 +0200

    azure api

--- iolock-webapp/src/environments/environment.development.ts
+++ iolock-webapp/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl : 'https://localhost:7117/api',
+  apiUrl : 'https://purple-meadow-05135ff03.3.azurestaticapps.net/api',
   keycloakUrl:'https://iolock-keycloak.azurewebsites.net/auth/'
 };
